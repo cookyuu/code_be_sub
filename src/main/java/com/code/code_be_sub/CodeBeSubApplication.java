d4e64aa675e28a62094ad0ac045d7d974e13f31f
임해규 <limk9500@gmail.com>
2025-02-24 00:28:20 +0900

    Initial commit

---
+++ src/main/java/com/code/code_be_sub/CodeBeSubApplication.java
@@ -0,0 +1,13 @@
+package com.code.code_be_sub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeBeSubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeBeSubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/code/code_be_sub/CodeBeSubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.code_be_sub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeBeSubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
